b2b6860940d0fff5cfd987904dd7ac8aca0fda19
JuanSena <juandsena1@gmail.com>
2024-07-23 22:59:49 -0300

    Projeto Calculadora do IPhone

--- calculadora.py
+++ calculadora.py
@@ -1,69 +1,73 @@
+# Importa a biblioteca Flet e a sub-biblioteca colors como clr
 import flet as ft 
 from flet import colors as clr
 from decimal import Decimal
 
+# Define os botões da calculadora com suas propriedades: operador, cor da fonte e cor de fundo
 bottons = [
-    {'operator': 'AC', 'fonte':clr.BLACK, 'fundo':clr.BLUE_GREY_100},
-    {'operator': '±', 'fonte':clr.BLACK, 'fundo':clr.BLUE_GREY_100},
-    {'operator': '%', 'fonte':clr.BLACK, 'fundo':clr.BLUE_GREY_100},
-    {'operator': '/', 'fonte':clr.WHITE, 'fundo':clr.ORANGE},
-    {'operator': '7', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '8', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '9', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '*', 'fonte':clr.WHITE, 'fundo':clr.ORANGE},
-    {'operator': '4', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '5', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '6', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '-', 'fonte':clr.WHITE, 'fundo':clr.ORANGE},
-    {'operator': '1', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '2', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '3', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '+', 'fonte':clr.WHITE, 'fundo':clr.ORANGE},
-    {'operator': '0', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '.', 'fonte':clr.WHITE, 'fundo':clr.WHITE24},
-    {'operator': '=', 'fonte':clr.WHITE, 'fundo':clr.ORANGE},
+    {'operator': 'AC', 'fonte': clr.BLACK, 'fundo': clr.BLUE_GREY_100},
+    {'operator': '±', 'fonte': clr.BLACK, 'fundo': clr.BLUE_GREY_100},
+    {'operator': '%', 'fonte': clr.BLACK, 'fundo': clr.BLUE_GREY_100},
+    {'operator': '/', 'fonte': clr.WHITE, 'fundo': clr.ORANGE},
+    {'operator': '7', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '8', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '9', 'fundo': clr.WHITE, 'fonte': clr.WHITE24},
+    {'operator': '*', 'fonte': clr.WHITE, 'fundo': clr.ORANGE},
+    {'operator': '4', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '5', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '6', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '-', 'fonte': clr.WHITE, 'fundo': clr.ORANGE},
+    {'operator': '1', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '2', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '3', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '+', 'fonte': clr.WHITE, 'fundo': clr.ORANGE},
+    {'operator': '0', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '.', 'fonte': clr.WHITE, 'fundo': clr.WHITE24},
+    {'operator': '=', 'fonte': clr.WHITE, 'fundo': clr.ORANGE},
 ]
 
-
-#Create a window
+# Função principal que configura a página da aplicação
 def main(page: ft.Page):
-    page.bgcolor = '#000' # -> background black 
+    # Configurações da janela da aplicação
+    page.bgcolor = '#000' 
     page.window_resizable = False
     page.window_width = 300
     page.window_height = 390
     page.title = 'Calculadora'
     page.window_always_on_top = True
     
-    result =  ft.Text(value = '0', color= clr.WHITE, size=25)
+    # Elemento de texto para mostrar o resultado
+    result = ft.Text(value='0', color=clr.WHITE, size=25)
     
+    # Função para realizar cálculos básicos
     def calculate(operator, value_at):
-        
         try:    
             value = eval(value_at)
-            if operator  == '%':
+            if operator == '%':
                 value /= 100
             elif operator == '±':
                 value = -value
         except:        
             return 'Error'
         
-        digits = min(abs(Decimal(value).as_tuple().exponent), 5)  
-        return format(value, f'.{digits}f') 
-        
+        # Limita o número de dígitos decimais a 5
+        digits = min(abs(Decimal(value).as_tuple().exponent), 5)
+        return format(value, f'.{digits}f')
     
+    # Função para manipular a seleção de botões
     def select(e):
         value_at = result.value if result.value not in ('0', 'Error') else ''
         value = e.control.content.value
         
         if value.isdigit():
-            value = value_at + value 
+            value = value_at + value
         elif value == 'AC':
             value = '0'
         else:
             if value_at and value_at[-1] in ('/', '*', '-', '+', '.'):
                 value_at = value_at[:-1]
                 
-            value = value_at + value 
+            value = value_at + value
             
             if value[-1] in ('=', '%', '±'):
                 value = calculate(operator=value[-1], value_at=value_at)
@@ -71,41 +75,35 @@ def main(page: ft.Page):
         result.value = value
         result.update()
     
-    
-        
-    #create botton
+    # Criação dos botões como contêineres
     btn = [ft.Container(
         content=ft.Text(value=btn['operator'], color=btn['fonte']),
         alignment=ft.alignment.center,
         width=50,
         height=50,
-        bgcolor= btn['fundo'],
+        bgcolor=btn['fundo'],
         border_radius=100,
-        on_click= select
-    )   for btn in bottons  ]
-    
-    
-    #define calculator number will be on the right side
+        on_click=select
+    ) for btn in bottons]
+
+    # Configura a linha de exibição do resultado
     display = ft.Row(
         width=250,
-        controls=[result], #row elements
-        alignment=  'end'
-    ) 
-    
-    
-    Keyboard =ft.Row(
-        width=250,
-        wrap=True,#causes the buttons to have a break and be one below the other
-        controls=btn,
-        alignment='end'    
+        controls=[result], 
+        alignment='end'
     )
-        
     
+    # Configura a linha de botões do teclado da calculadora
+    Keyboard = ft.Row(
+        width=250,
+        wrap=True,
+        controls=btn,
+        alignment='end'
+    )
     
-
+    # Adiciona a linha de exibição e o teclado à página
     page.add(display)
     page.add(Keyboard)
-    
-    
 
-ft.app(target = main)
+# Executa a aplicação chamando a função principal
+ft.app(target=main)
